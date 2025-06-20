55f38b0976ddf3623ca82791c96e76c89804a238
Wesley <wesleynjerenga9@gmail.com>
2025-06-20 15:21:51 +0300

    feat: add endpoint to create a new episode

--- server/controllers/episode_controller.py
+++ server/controllers/episode_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from ..models.episode import Episode
 from ..models import db
 
@@ -8,3 +8,15 @@ episode_bp = Blueprint('episode', __name__)
 def list_episodes():
     episodes = Episode.query.all()
     return jsonify([e.to_dict() for e in episodes]), 200
+
+@episode_bp.route('/episodes', methods=['POST'])
+def create_episode():
+    data = request.get_json()
+    title = data.get('title')
+    air_date = data.get('air_date')
+    if not title or not air_date:
+        return {"error": "Missing title or air_date"}, 400
+    episode = Episode(title=title, air_date=air_date)
+    db.session.add(episode)
+    db.session.commit()
+    return episode.to_dict(), 201
