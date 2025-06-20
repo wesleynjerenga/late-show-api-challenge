ab4cf861101e2eadfedb7ad94b855683ad74f80e
Wesley <wesleynjerenga9@gmail.com>
2025-06-20 15:22:06 +0300

    feat: add endpoint to create a new guest

--- server/controllers/guest_controller.py
+++ server/controllers/guest_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from ..models import Guest
 
 guest_bp = Blueprint('guests', __name__)
@@ -10,3 +10,16 @@ def get_guests():
         {'id': g.id, 'name': g.name, 'occupation': g.occupation}
         for g in guests
     ])
+
+@guest_bp.route('/guests', methods=['POST'])
+def create_guest():
+    data = request.get_json()
+    name = data.get('name')
+    occupation = data.get('occupation')
+    if not name or not occupation:
+        return {"error": "Missing name or occupation"}, 400
+    from ..models.guest import Guest, db
+    guest = Guest(name=name, occupation=occupation)
+    db.session.add(guest)
+    db.session.commit()
+    return {'id': guest.id, 'name': guest.name, 'occupation': guest.occupation}, 201
